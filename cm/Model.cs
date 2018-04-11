4bb8b800e11fd6f48252ef3f54713dbd4fe22af9
Dmitry Navrotsky <dnavrotsky@plazius.com>
2018-04-11 15:34:00 +0300

    Метки, сборка заголовка, упорядочивание авторов

--- cm/Model.cs
+++ cm/Model.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -20,12 +21,48 @@ namespace cm
             }
         }
 
+        private class BuildResult
+        {
+            public string Filename { get; }
+            public List<string> Labels { get; } = new List<string>();
+            public List<string> Authors { get; } = new List<string>();
+
+            public BuildResult(string file)
+            {
+                Filename = file;
+            }
+
+            public void Add(string label, string author)
+            {
+                Labels.Add(label);
+                Authors.Add(author);
+            }
+        }
+
+        private class AuthLabel
+        {
+            public string Author { get; }
+            public List<string> Labels { get; }
+
+            public AuthLabel(string name, string label)
+            {
+                Author = name;
+                Labels = new List<string>{ label };
+            }
+
+            public override string ToString()
+            {
+                return $"\\\\{Author} \\dotfill {string.Join(", ", Labels.Select(x => $"\\pageref{{{x}}}"))}";
+            }
+        }
+
         public List<string> Files { get; } = new List<string>();
 
         public string DefaultTarget => $@"Сборник {DateTime.Today:yyyy-MM-dd}.tex";
 
         #region . Build .
 
+        [SuppressMessage("ReSharper", "AssignNullToNotNullAttribute")]
         public bool Build(string header, string target)
         {
             if (string.IsNullOrEmpty(header))
@@ -37,19 +74,39 @@ namespace cm
 
             try
             {
-                var nameMap = new List<AuthFile>();
-                var body = Path.ChangeExtension(target, "body");
-                if (File.Exists(body))
-                    File.Delete(body);
-
-                Log.Info($"Временный файл: {body}");
+                var buildMap = new List<BuildResult>();
+                var labelMap = new List<AuthLabel>();
+                _index = 100;//исключительно для красоты
 
                 foreach (var file in Files)
                 {
                     Log.Info($"Сборка {file}");
-                    var key = BuildFile(file, body);
-                    nameMap.Add(new AuthFile(key, body));
+                    var body = Path.ChangeExtension(file, "converted.tex");
+                    if (File.Exists(body))
+                        File.Delete(body);
+
+                    Log.Info($"Временный файл: {body}");
+                    var result = BuildFile(file, body, labelMap);
+                    buildMap.Add(result);
                 }
+
+                /*
+                {
+                    Log.Info("Файлы, авторы и метки:");
+                    buildMap.ForEach(x =>
+                    {
+                        Log.Info($"\\input{{{Path.GetFileNameWithoutExtension(x.Filename)}}}");
+                        Log.Info(
+                            $"{string.Join(" ", x.Labels.Select(y => $"\\label{{{y}}}"))} % {string.Join(" % ", x.Authors)}");
+                    });
+
+                    Log.Info("Авторы и метки:");
+                    foreach (var item in labelMap.OrderBy(x => x.Author))
+                    {
+                        Log.Info(item.ToString());
+                    }
+                }
+                */
 
                 using (var output = File.Create(target))
                 {
@@ -61,20 +118,33 @@ namespace cm
                         output.Write(bytes, 0, bytes.Length);
                     }
 
-                    using (var b = File.OpenRead(body))
+                    foreach (var item in buildMap)
                     {
-                        Log.Info($"Вывод статей {body}");
-                        var bytes = new byte[b.Length];
-                        b.Read(bytes, 0, bytes.Length);
-                        output.Write(bytes, 0, bytes.Length);
+                        PutString(output, "%");
+                        PutString(output, "\\pagebreak");
+                        PutString(output, "\\setcounter{equation}{0}");
+                        PutString(output, $"{string.Join(" ", item.Labels.Select(y => $"\\label{{{y}}}"))} % {string.Join(" % ", item.Authors)}");
+                        PutString(output, $"\\input{{{Path.GetFileNameWithoutExtension(item.Filename)}}}");
                     }
 
-                    File.Delete(body);
-                    Log.Info($"Временный файл {body} удален");
+                    PutString(output, "");
+                    PutString(output, "%------------------------------------------------------------------------------");
+                    PutString(output, "\\pagebreak");
+                    PutString(output, "\\markboth{ }{ }");
+                    PutString(output, "\\vspace{-10mm}");
+                    PutString(output, "\\begin{center} СПИСОК АВТОРОВ \\end{center}");
+                    PutString(output, "\\vspace{-5mm}");
+                    PutString(output, "\\label{List}");
+                    PutString(output, "\\vspace{5mm}");
+                    PutString(output, "\\noindent");
+
+                    foreach (var item in labelMap.OrderBy(x => InterpretForSorting(x.Author)))
+                    {
+                        PutString(output, item.ToString());
+                    }
 
                     {
-                        var bytes = Encoding.GetEncoding(1251).GetBytes("\\end{document}");
-                        output.Write(bytes, 0, bytes.Length);
+                        PutString(output, "\\end{document}");
                         output.Flush();
                     }
                     Log.Info($"Сборка {target} завершена");
@@ -90,20 +160,32 @@ namespace cm
             }
         }
 
+        private string InterpretForSorting(string s)
+        {
+            return (s[0] >= 'A' && s[0] <= 'Z')
+                ? "ЯЯЯ" + s
+                : s;
+        }
+
+        private void PutString(FileStream stream, string s)
+        {
+            var bytes = Encoding.UTF8.GetBytes(s + "\n");
+            stream.Write(bytes, 0, bytes.Length);
+        }
+
         private const string TitleTag = "\\title";
 
         private const string FinalTag = "\\end";
 
-        private string BuildFile(string file, string target)
+        private int _index;
+
+        private BuildResult BuildFile(string file, string target, List<AuthLabel> labelMap)
         {
+            var result = new BuildResult(target);
             var builder = new StringBuilder(256);
 
             using (var output = File.AppendText(target))
             {
-                output.WriteLine("%");
-                output.WriteLine($"% --==[ {file} ]==--");
-                output.WriteLine("%");
-
                 using (var reader = new StreamReader(file, Encoding.GetEncoding(1251)))
                 {
                     var body = false;
@@ -113,11 +195,10 @@ namespace cm
                         if (string.IsNullOrEmpty(token))
                             continue;
 
-                        if (token == "\\")
-                            token = string.Concat(token, GetToken(reader));
-
                         if (token.Trim().ToLower() == TitleTag)
                             body = true;
+
+                        #region . check for \end{document} .
 
                         if (token.Trim().ToLower() == FinalTag)
                         {
@@ -144,6 +225,10 @@ namespace cm
                         else if (body)
                             output.Write(token);
 
+                        #endregion
+
+                        #region . list authors .
+
                         if (token.Trim().ToLower() == AuthorTag)
                         {
                             while (token != "{")
@@ -155,25 +240,72 @@ namespace cm
                             token = GetToken(reader);
                             output.Write(token);
 
-                            while (token != "\\" && token != "}")
+                            while (token != "}")
                             {
+                                if (token.Trim().StartsWith("\\"))
+                                {
+                                    var author = builder.ToString().Trim(' ', ',');
+                                    if (!string.IsNullOrWhiteSpace(author))
+                                    {
+                                        var label = $"lbl{++_index}";
+                                        builder.Clear();
+
+                                        var al = labelMap.FirstOrDefault(x => x.Author == author);
+                                        if (al != null)
+                                            al.Labels.Add(label);
+                                        else
+                                            labelMap.Add(new AuthLabel(author, label));
+                                        result.Add(label, author);
+                                    }
+
+                                    token = GetToken(reader);
+                                    output.Write(token);
+
+                                    continue;
+                                }
+
+                                if (token == "{")
+                                {
+                                    while (token != "}")
+                                        token = GetToken(reader);
+                                    token = GetToken(reader);
+                                    output.Write(token);
+
+                                    continue;
+                                }
+
                                 builder.Append(token.Replace("\r\n", " ").Replace("\r", " ").Replace("\n", " ")
                                     .Replace("\t", " "));
+
                                 token = GetToken(reader);
                                 output.Write(token);
                             }
                         }
+
+                        //Последнего автора
+                        if (builder.Length > 0)
+                        {
+                            var label = $"lbl{++_index}";
+
+                            var author = builder.ToString().Trim();
+
+                            var al = labelMap.FirstOrDefault(x => x.Author == author);
+                            if (al != null)
+                                al.Labels.Add(label);
+                            else
+                                labelMap.Add(new AuthLabel(author, label));
+                            result.Add(label, author);
+                        }
+
+                        #endregion
+
                     }
                 }
 
                 output.Flush();
             }
 
-            var result = builder.ToString().Trim();
-
-            return (result[0] > 'а' && result[0] < 'я')
-                ? CyrillicName(result)
-                : LatinName(result);
+            return result;
         }
 
         #endregion
@@ -219,9 +351,6 @@ namespace cm
                     if (string.IsNullOrEmpty(token))
                         continue;
 
-                    if (token == "\\")
-                        token = string.Concat(token, GetToken(reader));
-
                     if (token.Trim().ToLower() == AuthorTag)
                     {
                         while (token != "{")
@@ -229,7 +358,7 @@ namespace cm
 
                         token = GetToken(reader);
 
-                        while (token != "\\" && token != "}")
+                        while (!token.StartsWith("\\") && token != "}")
                         {
                             builder.Append(token.Replace("\r\n", " ").Replace("\r", " ").Replace("\n", " ").Replace("\t", " "));
                             token = GetToken(reader);
@@ -271,7 +400,9 @@ namespace cm
             var result = new StringBuilder(256);
             result.Append(c);
             if (_delimeter.Contains(c))
-                return result.ToString();
+                return c == '\\'
+                    ? string.Concat(result, GetToken(reader))
+                    : result.ToString();
 
             while (!reader.EndOfStream)
             {
